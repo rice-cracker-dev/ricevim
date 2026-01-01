14455c449a559b0c1e7df6e8eb1bc4e7cf9dd14e
rice-cracker-dev <ricecracker2234@proton.me>
2026-01-01 16:26:13 +0700

    feat(snacks.nvim): add dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -52,6 +52,16 @@ return {
         padding = false,
         gap = 1,
       },
+      dashboard = {
+        enabled = true,
+        pane_gap = 16,
+        sections = {
+          { section = 'keys', gap = 1, padding = 1 },
+          { pane = 2, icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
+          { pane = 2, icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
+          { section = 'startup' },
+        },
+      },
     },
   },
 }
