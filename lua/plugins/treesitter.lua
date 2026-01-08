e14f4c7b17547424cb72160ac5a5e0f324014017
rice-cracker-dev <ricecracker2234@proton.me>
2026-01-08 23:56:14 +0700

    feat: update flake inputs
    removed jsonc as nvim-treesitter no longer packages it
    https://github.com/nvim-treesitter/nvim-treesitter/pull/8316

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,37 +1,36 @@
 ---@module 'lazy'
+---@module 'nvim-treesitter'
 ---@type LazySpec
 return {
   'nvim-treesitter/nvim-treesitter',
   lazy = false,
 
-  config = function()
-    require('nvim-treesitter.configs').setup({
-      modules = {},
-      sync_install = false,
-      auto_install = false,
-      ignore_install = {},
-      ensure_installed = {},
+  opts = {
+    modules = {},
+    sync_install = false,
+    auto_install = false,
+    ignore_install = {},
+    ensure_installed = {},
 
-      highlight = {
-        enable = true,
-        disable = {},
-        additional_vim_regex_highlighting = false,
-      },
+    highlight = {
+      enable = true,
+      disable = {},
+      additional_vim_regex_highlighting = false,
+    },
 
-      indent = {
-        enable = true,
-        disable = {},
-      },
+    indent = {
+      enable = true,
+      disable = {},
+    },
 
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = 'gnn',
-          node_incremental = 'grn',
-          scope_incremental = 'grc',
-          node_decremental = 'grm',
-        },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = 'gnn',
+        node_incremental = 'grn',
+        scope_incremental = 'grc',
+        node_decremental = 'grm',
       },
-    })
-  end,
+    },
+  },
 }
--- nix/plugins.nix
+++ nix/plugins.nix
@@ -13,7 +13,6 @@
       javascript
       typescript
       json
-      jsonc
       svelte
       yaml
       css
