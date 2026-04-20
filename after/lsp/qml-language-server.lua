178b28d107a458866714056b01d0da2bdc67f0c0
rice-cracker-dev <ricecracker2234@proton.me>
2026-04-20 21:12:32 +0700

    lsp/qml-language-server: fix root_dir behavior

--- after/lsp/qml-language-server.lua
+++ after/lsp/qml-language-server.lua
@@ -1,15 +1,20 @@
+---@type vim.lsp.Config
 return {
   name = 'qml-language-server',
   filetypes = { 'qml' },
-  root_dir = function(fname)
+  root_dir = function(bufnr, ondir)
     local root_patterns = { '.git', '.qml', 'qmldir' }
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+
     for _, pattern in ipairs(root_patterns) do
       local root = vim.fs.find(pattern, { path = fname, upward = true })[1]
       if root then
-        return vim.fs.dirname(root)
+        ondir(vim.fs.dirname(root))
+        return
       end
     end
-    return vim.fs.dirname(fname)
+
+    ondir(vim.fs.dirname(fname))
   end,
   cmd = { 'qml-language-server' },
 }
